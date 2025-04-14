5deb7dff84a4f230ad67628f9356e8b35a07453a
Christian Putri <christianptrii@gmail.com>
2025-04-15 00:20:43 +0800

    Menambahkan note routes

--- routes/routes.go
+++ routes/routes.go
@@ -18,4 +18,12 @@ func SetupRoutes(r *gin.Engine) {
 	r.POST("/category", controller.CreateCategory)
 	r.PUT("/category/:id_category", controller.UpdateCategory)
 	r.DELETE("/category/:id_category", controller.DeleteCategory)
+
+	r.GET("/notes", controller.GetAllNotes)
+	r.GET("/notes/:id_notes", controller.GetNoteByID)
+	r.POST("/notes", controller.CreateNote)
+	r.PUT("/notes/:id_notes", controller.UpdateNote)
+	r.DELETE("/notes/:id_notes", controller.DeleteNote)
+	r.GET("/notes/category/:id_category", controller.GetNotesByCategory)
+	r.GET("/notes/favorite", controller.GetFavoriteNotes)
 }
