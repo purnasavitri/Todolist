5315ed732f8288962efa83f88ec3ef57a93f75b4
IndiraJayanti <165116625+IndiraJayanti@users.noreply.github.com>
2025-04-15 00:12:10 +0800

    Mengubah main.go

--- main.go
+++ main.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"todolist/config"
+	"todolist/routes"
 
 	"github.com/gin-gonic/gin"
 )
-
-type User struct {
-	ID       uint   `gorm:"column:id_user;primaryKey;table:user" json:"id"`
-	Name     string `gorm:"column:name" json:"name"`
-	Email    string `gorm:"column:email" json:"email"`
-	Password string `gorm:"column:password" json:"-"`
-}
-
-func (User) TableName() string {
-	return "user"
-}
 
 func main() {
 	// Menginisialisasi koneksi database
@@ -25,11 +14,8 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/user", func(c *gin.Context) {
-		var user []User
-		config.DB.Find(&user)
-		c.JSON(http.StatusOK, gin.H{"data": user})
-	})
+	// Setup routes
+	routes.SetupRoutes(r)
 
 	// Jalankan server di port 8080
 	if err := r.Run(":8080"); err != nil {
