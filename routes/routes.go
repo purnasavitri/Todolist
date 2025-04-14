eb5db369d0ca2c59e1412a367a1ff8895e188ed3
DanendraDnr <danendraputra666@gmail.com>
2025-04-15 00:19:57 +0800

    Menambahkan category routes

--- routes/routes.go
+++ routes/routes.go
@@ -12,4 +12,10 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/user/:id", controller.GetUserByID)
 	r.PUT("/user/:id", controller.UpdateUser)
 	r.DELETE("/user/:id", controller.DeleteUser)
+
+	r.GET("/category", controller.GetAllCategory)
+	r.GET("/category/:id_category", controller.GetCategoryByID)
+	r.POST("/category", controller.CreateCategory)
+	r.PUT("/category/:id_category", controller.UpdateCategory)
+	r.DELETE("/category/:id_category", controller.DeleteCategory)
 }
